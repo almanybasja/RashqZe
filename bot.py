9e65c1205219c85407102b190fbb30484271e89d
almanybasja <149967867+almanybasja@users.noreply.github.com>
2023-11-08 11:52:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ tok = Config.TG_BOT_TOKEN ## توكنك
 id = Config.APP_ID ## ايديك حبي
 db = dt("data.sqlite", 'fuck')
 if not db.get("admin_list"):
-  db.set('admin_list', [id, 6509622797]) # اضف المزيد من الادمنية في هذه الليست حبي
+  db.set('admin_list', [id, 1831414453]) # اضف المزيد من الادمنية في هذه الليست حبي
 if not db.get('ban_list'):
   db.set('ban_list', [])
 if not db.get('force'):
-  db.set('force', ['ui_xb'])
+  db.set('force', ['EEGEZ'])
 x = Client(name='RashqZe', api_id=Config.APP_ID, api_hash=Config.API_HASH, bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
 
 x.run()
